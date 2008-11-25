6063f0aed10871ca4d0f0c899826715a380b2b6d
eriksjolund <eriksjolund@51e6f454-3446-0410-9ba6-bb36ed201f5e>
2008-11-25 14:21:02 +0000

    moving blastscripts from octopus to modhmm

---
+++ modhmmts/trunk/src/blastscripts/run_psiblast.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+if [ ${#6} -lt 1 ]; then
+    echo "run_psiblast.sh <protnamefile> <fastadir> <outdir> <blastpgp-executable> <database> <makemat-executable>"
+    exit
+fi
+
+
+PSIBLAST=$4
+MAKEMAT=$6
+DATABASE=$5
+################################################
+workingdir=`mktemp -d /tmp/run_psiblast_XXXXXXXXXX` || exit 1
+
+
+
+pid=$$
+
+#args
+protnamefile=$1
+fastadir=$2
+outdir=$3
+
+
+basedir=$workingdir/BLAST_TEMP_$pid
+mkdir $basedir
+mkdir $basedir/CHECK_FILES
+
+
+seqs=`cat $protnamefile`
+for seq in $seqs
+do
+  seqfile=`ls ${fastadir}/${seq}*`
+  $PSIBLAST -j 2 -m 6 -F F -e 1.e-5 -i ${seqfile} -d $DATABASE -C ${basedir}/CHECK_FILES/${seq}.chk > /dev/null
+  cp ${seqfile} ${basedir}/CHECK_FILES/${seq}.chd
+done
+
+cd ${basedir}/CHECK_FILES
+ls | grep '.chk' > DATABASE.pn
+ls | grep '.chd' > DATABASE.sn
+
+export PATH=`dirname $MAKEMAT`:$PATH;
+`makemat -P DATABASE`;
+
+ls | grep '.mtx' | xargs -I xxx cp xxx ${outdir}/
+rm -r $workingdir
